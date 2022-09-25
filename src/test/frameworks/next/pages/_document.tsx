909bdd75d2232f67209445415ccafe1761ab720c
garronej <joseph.garrone.gj@gmail.com>
2022-09-25 22:32:41 +0200

    Document fonctional style

--- src/test/frameworks/next/pages/_document.tsx
+++ src/test/frameworks/next/pages/_document.tsx
@@ -4,27 +4,24 @@ import { getColorSchemeSsrUtils } from "dsfr-react/lib/nextJs";
 
 const { readColorSchemeFromCookie, getColorSchemeHtmlAttributes } = getColorSchemeSsrUtils();
 
-export default class Document extends DefaultDocument {
-	static async getInitialProps(ctx: DocumentContext) {
-
-		const initialProps = await DefaultDocument.getInitialProps(ctx);
-
-		readColorSchemeFromCookie(ctx);
-
-		return { ...initialProps };
-
-	}
-
-	render() {
-		return (
-			<Html {...getColorSchemeHtmlAttributes()}>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
-
+export default function Document() {
+	return (
+		<Html {...getColorSchemeHtmlAttributes()}>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+
+	const initialProps = await DefaultDocument.getInitialProps(ctx);
+
+	readColorSchemeFromCookie(ctx);
+
+	return { ...initialProps };
+
+};
