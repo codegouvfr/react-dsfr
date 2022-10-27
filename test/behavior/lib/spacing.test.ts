9e5431a63b1b5d46fd0665377464b79e2d9f73b5
garronej <joseph.garrone@data.gouv.fr>
2022-10-27 23:10:40 +0200

    Write runtime test for spacing

--- test/behavior/lib/spacing.test.ts
+++ test/behavior/lib/spacing.test.ts
@@ -9,4 +9,49 @@ describe("Testing the replacer function", () => {
 
         expect(got).toBe(expected);
     });
+
+    it("Works with margin topBottom", () => {
+        const got = spacing("margin", { "topBottom": "13w" });
+
+        const expected = {
+            "marginTop": "6.5rem",
+            "marginBottom": "6.5rem"
+        };
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("Works with margin topBottom and left", () => {
+        const got = spacing("margin", { "topBottom": "13w", "left": 3 });
+
+        const expected = {
+            "marginTop": "6.5rem",
+            "marginBottom": "6.5rem",
+            "marginLeft": 3
+        };
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("Works with margin topBottom overwriting bottom", () => {
+        const got = spacing("margin", { "topBottom": "13w", "bottom": 3 });
+
+        const expected = {
+            "marginTop": "6.5rem",
+            "marginBottom": 3
+        };
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("Works with padding and rightLeft", () => {
+        const got = spacing("padding", { "rightLeft": "1v" });
+
+        const expected = {
+            "paddingRight": "0.25rem",
+            "paddingLeft": "0.25rem"
+        };
+
+        expect(got).toStrictEqual(expected);
+    });
 });
