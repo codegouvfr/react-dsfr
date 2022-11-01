c7547c723fc83662779747f0625fbe7ab7d087c7
garronej <joseph.garrone@data.gouv.fr>
2022-11-01 17:02:43 +0100

    Fix cx

--- src/lib/cx.ts
+++ src/lib/cx.ts
@@ -13,7 +13,7 @@ export type FrCxArg =
 /** Copy pasted from
  * https://github.com/emotion-js/emotion/blob/23f43ab9f24d44219b0b007a00f4ac681fe8712e/packages/react/src/class-names.js#L17-L63
  **/
-export const cx = (args: FrCxArg[]): string => {
+export const cx = (...args: FrCxArg[]): string => {
     const len = args.length;
     let i = 0;
     let cls = "";
