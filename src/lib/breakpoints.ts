f5ab4375b2a9b50e166418f1ef5835eeaefd4425
garronej <joseph.garrone@data.gouv.fr>
2022-11-11 17:25:07 +0100

    Better breakpoint utility

--- src/lib/breakpoints.ts
+++ src/lib/breakpoints.ts
@@ -4,14 +4,17 @@ import {
     breakpointValuesUnit as unit,
     breakpointKeys as keys
 } from "./generatedFromCss/breakpoints";
+import { assert } from "tsafe/assert";
 
 export type { BreakpointKeys };
 
 const epsilon = 0.003125;
 
 export const breakpoints = {
-    "up": (key: BreakpointKeys) => `@media (min-width:${values[key]}${unit})` as const,
-    "down": (key: BreakpointKeys) => `@media (max-width:${values[key] - epsilon}${unit})` as const,
+    "up": (key: Exclude<BreakpointKeys, "xs">) =>
+        `@media (min-width:${values[key]}${unit})` as const,
+    "down": (key: Exclude<BreakpointKeys, "xs">) =>
+        `@media (max-width:${values[key] - epsilon}${unit})` as const,
     "between": (start: BreakpointKeys, end: BreakpointKeys) =>
         `@media (min-width:${values[start]}${unit}) and (max-width:${
             values[end] - epsilon
@@ -19,7 +22,7 @@ export const breakpoints = {
     "only": (key: BreakpointKeys) =>
         keys.indexOf(key) + 1 < keys.length
             ? breakpoints.between(key, keys[keys.indexOf(key) + 1])
-            : breakpoints.up(key),
+            : breakpoints.up((assert(key !== "xs"), key)),
     "not": (key: BreakpointKeys) => {
         // handle first and last key separately, for better readability
         const keyIndex = keys.indexOf(key);
@@ -27,7 +30,9 @@ export const breakpoints = {
             return breakpoints.up(keys[1]);
         }
         if (keyIndex === keys.length - 1) {
-            return breakpoints.down(keys[keyIndex]);
+            const key = keys[keyIndex];
+            assert(key !== "xs");
+            return breakpoints.down(key);
         }
 
         return breakpoints
