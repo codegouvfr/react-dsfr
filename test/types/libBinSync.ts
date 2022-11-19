7248cb25d4f51661b6c7854469ab1f0c968a65d8
garronej <joseph.garrone@data.gouv.fr>
2022-11-19 20:13:29 +0100

    Update type concistency test

--- test/types/libBinSync.ts
+++ test/types/libBinSync.ts
@@ -4,12 +4,7 @@ so when we copy declaration values we make sure they stay in sync here
 */
 import { assert } from "tsafe/assert";
 import type { Equals } from "tsafe";
-import type {
-    data_fr_theme as data_fr_theme_lib,
-    ColorScheme as ColorScheme_lib
-} from "../../src/lib/darkMode";
-import type { data_fr_theme as data_fr_theme_bin } from "../../src/bin/css_to_ts/cssVariable";
-import type { ColorScheme as ColorScheme_bin } from "../../src/bin/css_to_ts/colorOptions";
+import type { ColorScheme as ColorScheme_lib } from "../../src/lib/darkMode";
+import type { ColorScheme as ColorScheme_bin } from "../../src/scripts/cssToTs/colorOptions";
 
-assert<Equals<typeof data_fr_theme_bin, typeof data_fr_theme_lib>>();
 assert<Equals<ColorScheme_lib, ColorScheme_bin>>();
