bdc31458a5d7cebfeb496249d552acb0057c7853
garronej <joseph.garrone@data.gouv.fr>
2022-11-20 15:04:30 +0100

    Enable to disable builtin props

--- docs/DarkModeSwitch.stories.tsx
+++ docs/DarkModeSwitch.stories.tsx
@@ -6,8 +6,14 @@ const { meta, getStory } = getStoryFactory({
     sectionName,
     "wrappedComponent": { DarkModeSwitch },
     "description": `
+A button that opens a dialog to enable the user to select light or dark mode.  
+
 - [See DSFR documentation](https://www.systeme-de-design.gouv.fr/elements-d-interface/composants/parametres-d-affichage),
-- [See source code](https://github.com/codegouvfr/react-dsfr/blob/main/src/DarkModeSwitch.tsx)`,
+- [See source code](https://github.com/codegouvfr/react-dsfr/blob/main/src/DarkModeSwitch.tsx)
+
+Optionally, you can also use \`import { useIsDark } from "@codegouvfr/react-dsfr"\` to manually monitor and controls 
+the theme state.
+`,
     "disabledProps": ["darkMode", "containerWidth"]
 });
 
