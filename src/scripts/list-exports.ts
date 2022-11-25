82235f6478e0096930635b1c71ec08a9acf066bc
garronej <joseph.garrone@data.gouv.fr>
2022-11-25 18:37:35 +0100

    Improve list-exports

--- src/scripts/list-exports.ts
+++ src/scripts/list-exports.ts
@@ -2,10 +2,50 @@ import * as fs from "fs";
 import { join as pathJoin } from "path";
 import { getProjectRoot } from "../bin/tools/getProjectRoot";
 import { exclude } from "tsafe/exclude";
+import { execSync } from "child_process";
+import { same } from "evt/tools/inDepth/same";
 
 const packageJsonFilePath = pathJoin(getProjectRoot(), "package.json");
 
 const packageJsonParsed = JSON.parse(fs.readFileSync(packageJsonFilePath).toString("utf8"));
+
+const srcDirPath = pathJoin(getProjectRoot(), "src");
+
+const newExports = {
+    ".": `./${packageJsonParsed["module"]}`,
+    ...Object.fromEntries(
+        fs
+            .readdirSync(srcDirPath)
+            .filter(basename => {
+                try {
+                    execSync(`git ls-files --error-unmatch ${pathJoin(srcDirPath, basename)}`, {
+                        "stdio": []
+                    });
+                } catch {
+                    return false;
+                }
+
+                return true;
+            })
+            .map(basename => {
+                const match = basename.match(/^([^.]+)\.tsx?$/);
+
+                if (match === null) {
+                    return undefined;
+                }
+
+                return match[1];
+            })
+            .filter(exclude(undefined))
+            .sort()
+            .reverse()
+            .map(name => [`./${name}`, `./dist/${name}.js`])
+    )
+};
+
+if (same(packageJsonParsed["exports"], newExports)) {
+    process.exit(0);
+}
 
 fs.writeFileSync(
     packageJsonFilePath,
@@ -13,26 +53,7 @@ fs.writeFileSync(
         JSON.stringify(
             {
                 ...packageJsonParsed,
-                "exports": {
-                    ".": `./${packageJsonParsed["module"]}`,
-                    ...Object.fromEntries(
-                        fs
-                            .readdirSync(pathJoin(getProjectRoot(), "src"))
-                            .map(basename => {
-                                const match = basename.match(/^([^.]+)\.tsx?$/);
-
-                                if (match === null) {
-                                    return undefined;
-                                }
-
-                                return match[1];
-                            })
-                            .filter(exclude(undefined))
-                            .sort()
-                            .reverse()
-                            .map(name => [`./${name}`, `./dist/${name}.js`])
-                    )
-                }
+                "exports": newExports
             },
             null,
             2
