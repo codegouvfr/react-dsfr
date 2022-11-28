b8ee1233bd03a123b186c2ab493072ea846eb669
garronej <joseph.garrone@data.gouv.fr>
2022-11-28 13:31:39 +0100

    Partial copy paste of the MUI Theme type definition

---
+++ src/lib/tools/@mui/material/styles/createMixins.ts
@@ -0,0 +1,7 @@
+
+import type { CSSProperties } from "@mui/material/styles/createMixins";
+
+export interface Mixins {
+  toolbar: CSSProperties;
+  // ... use interface declaration merging to add custom mixins
+}
