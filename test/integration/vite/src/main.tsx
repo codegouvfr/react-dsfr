e8b78dd5ad069a322fbcc34b34b25d4ac8214e34
garronej <joseph.garrone@data.gouv.fr>
2022-11-30 20:10:31 +0100

    Remove white space

--- test/integration/vite/src/main.tsx
+++ test/integration/vite/src/main.tsx
@@ -50,7 +50,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/mui" element={<Mui />} />
-
                         <Route path="*" element={<h1>404</h1>} />
                     </Routes>
                 </div>
