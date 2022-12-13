0bddcd1f464ef2a2dcd8d0d76734a78718e34227
garronej <joseph.garrone@data.gouv.fr>
2022-12-13 19:06:06 +0100

    Support vite publicDir config

--- test/integration/vite/vite.config.ts
+++ test/integration/vite/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    publicDir: "public",
     server: {
         fs: {
           allow: ['../../..']
