cc376e835e57a43c095bb051d1be6a8a1bb6f2e1
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-12-15 12:41:01 +0100

    feat: add Quote component

---
+++ src/Quote.tsx
@@ -0,0 +1,54 @@
+import React, { memo, forwardRef, ReactNode } from "react";
+import { symToStr } from "tsafe/symToStr";
+import { assert } from "tsafe/assert";
+import type { Equals } from "tsafe";
+import { fr } from "./lib";
+import { cx } from "./lib/tools/cx";
+
+// We make users import dsfr.css, so we don't need to import the scoped CSS
+// but in the future if we have a complete component coverage it
+// we could stop requiring users to import the hole CSS and only import on a
+// per component basis.
+import "./dsfr/component/quote/quote.css";
+
+export type QuoteProps = {
+    className?: string;
+    text: string;
+    author?: string;
+    source?: ReactNode;
+    sourceUrl?: string;
+    image?: string;
+};
+
+/** @see <https://react-dsfr-components.etalab.studio/?path=/docs/components-stepper> */
+export const Quote = memo(
+    forwardRef<HTMLDivElement, QuoteProps>((props, ref) => {
+        const { className, text, author, source, sourceUrl, image, ...rest } = props;
+
+        assert<Equals<keyof typeof rest, never>>();
+
+        return (
+            <figure
+                className={cx(fr.cx("fr-quote"), fr.cx("fr-quote--column"), className)}
+                ref={ref}
+            >
+                <blockquote cite={sourceUrl}>
+                    <p>« {text} »</p>
+                </blockquote>
+                <figcaption>
+                    {author && <p className="fr-quote__author">{author}</p>}
+                    {source && <ul className="fr-quote__source">{source}</ul>}
+                    {image && (
+                        <div className="fr-quote__image">
+                            <img src={image} className="fr-responsive-img" alt="" />
+                        </div>
+                    )}
+                </figcaption>
+            </figure>
+        );
+    })
+);
+
+Quote.displayName = symToStr({ Quote });
+
+export default Quote;
---
+++ stories/Quote.stories.tsx
@@ -0,0 +1,74 @@
+import { Quote } from "../dist/Quote";
+import { sectionName } from "./sectionName";
+import { getStoryFactory } from "./getStory";
+
+const { meta, getStory } = getStoryFactory({
+    sectionName,
+    "wrappedComponent": { Quote },
+    "description": `
+- [See DSFR documentation](https://www.systeme-de-design.gouv.fr/elements-d-interface/composants/citation)
+- [See source code](https://github.com/codegouvfr/react-dsfr/blob/main/src/Quote.tsx)`,
+    "disabledProps": ["lang"]
+});
+
+export default meta;
+
+export const Default = getStory({
+    text: "Lorem [...] elit ut. ",
+    author: "Auteur",
+    source: (
+        <>
+            {" "}
+            <li>
+                <cite>Ouvrage</cite>
+            </li>
+            <li>Détail 1</li>
+            <li>Détail 2</li>
+            <li>Détail 3</li>
+            <li>
+                <a
+                    target="_blank"
+                    href="[À MODIFIER | Lien vers la sources ou des infos complémentaires]"
+                >
+                    Détail 4
+                </a>
+            </li>
+        </>
+    ),
+    image: "https://www.systeme-de-design.gouv.fr/img/placeholder.1x1.png"
+});
+
+export const QuoteWithoutDetails = getStory({
+    text: "Lorem [...] elit ut. ",
+    author: "Auteur",
+    image: "https://www.systeme-de-design.gouv.fr/img/placeholder.1x1.png"
+});
+
+export const QuoteWithoutSource = getStory({
+    text: "Lorem [...] elit ut. ",
+    image: "https://www.systeme-de-design.gouv.fr/img/placeholder.1x1.png"
+});
+
+export const QuoteWithoutIllustration = getStory({
+    text: "Lorem [...] elit ut. ",
+    author: "Auteur",
+    source: (
+        <>
+            {" "}
+            <li>
+                <cite>Ouvrage</cite>
+            </li>
+            <li>Détail 1</li>
+            <li>Détail 2</li>
+            <li>Détail 3</li>
+            <li>
+                <a
+                    target="_blank"
+                    href="[À MODIFIER | Lien vers la sources ou des infos complémentaires]"
+                >
+                    Détail 4
+                </a>
+            </li>
+        </>
+    )
+});
