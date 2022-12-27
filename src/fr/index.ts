4b9de29fc7b36b1ad89eb49d09967d9cc7a7757a
garronej <joseph.garrone@data.gouv.fr>
2022-12-27 20:21:34 +0100

    Export getColors in fr utils

--- src/fr/index.ts
+++ src/fr/index.ts
@@ -5,9 +5,12 @@ import { spacing } from "./spacing";
 export type { SpacingToken } from "./spacing";
 import { cx } from "./cx";
 export type { FrCxArg } from "./cx";
+export type { ColorTheme } from "./colors";
+import { getColors } from "./colors";
 
 export const fr = {
     breakpoints,
     spacing,
-    cx
+    cx,
+    getColors
 };
