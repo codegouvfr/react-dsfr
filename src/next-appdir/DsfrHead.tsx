aaad2edb304920a927010faae924f248d6143e4d
garronej <joseph.garrone@data.gouv.fr>
2023-01-03 12:14:21 +0100

    Refactor head imports

--- src/next-appdir/DsfrHead.tsx
+++ src/next-appdir/DsfrHead.tsx
@@ -1,40 +1,14 @@
 import React from "react";
 import { objectKeys } from "tsafe/objectKeys";
-//import { getColors } from "./lib/colors";
 import type { ColorScheme } from "../useIsDark";
 import { getAssetUrl } from "../tools/getAssetUrl";
-//import { rootColorSchemeStyleTagId } from "./lib/darkMode";
-
-import marianneLightWoff2Url from "../dsfr/fonts/Marianne-Light.woff2";
-import marianneItalicWoff2Url from "../dsfr/fonts/Marianne-Light_Italic.woff2";
-import marianneRegularWoff2Url from "../dsfr/fonts/Marianne-Regular.woff2";
-import marianneRegularItalicWoff2Url from "../dsfr/fonts/Marianne-Regular_Italic.woff2";
-import marianneMediumWoff2Url from "../dsfr/fonts/Marianne-Medium.woff2";
-import marianneMediumItalicWoff2Url from "../dsfr/fonts/Marianne-Medium_Italic.woff2";
-import marianneBoldWoff2Url from "../dsfr/fonts/Marianne-Bold.woff2";
-import marianneBoldItalicWoff2Url from "../dsfr/fonts/Marianne-Bold_Italic.woff2";
-import spectralRegularWoff2Url from "../dsfr/fonts/Spectral-Regular.woff2";
-import spectralExtraBoldWoff2Url from "../dsfr/fonts/Spectral-ExtraBold.woff2";
 import AppleTouchIcon from "../dsfr/favicon/apple-touch-icon.png";
 import FaviconSvg from "../dsfr/favicon/favicon.svg";
 import FaviconIco from "../dsfr/favicon/favicon.ico";
-import { data_fr_scheme, data_fr_theme, rootColorSchemeStyleTagId } from "../useIsDark/constants";
-import { getColors } from "../fr/colors";
+import { getScriptToRunAsap } from "../useIsDark/scriptToRunAsap";
+import { fontUrlByFileBasename } from "./fontUrlByFileBasename";
 import "../dsfr/dsfr.css";
 import "../dsfr/utility/icons/icons.css";
-
-const fontUrlByFileBasename = {
-    "Marianne-Light": marianneLightWoff2Url,
-    "Marianne-Light_Italic": marianneItalicWoff2Url,
-    "Marianne-Regular": marianneRegularWoff2Url,
-    "Marianne-Regular_Italic": marianneRegularItalicWoff2Url,
-    "Marianne-Medium": marianneMediumWoff2Url,
-    "Marianne-Medium_Italic": marianneMediumItalicWoff2Url,
-    "Marianne-Bold": marianneBoldWoff2Url,
-    "Marianne-Bold_Italic": marianneBoldItalicWoff2Url,
-    "Spectral-Regular": spectralRegularWoff2Url,
-    "Spectral-ExtraBold": spectralExtraBoldWoff2Url
-} as const;
 
 export type DsfrHeadProps = {
     defaultColorScheme: ColorScheme | "system";
@@ -69,94 +43,8 @@ export function DsfrHead(props: DsfrHeadProps) {
             <link rel="shortcut icon" href={getAssetUrl(FaviconIco)} type="image/x-icon" />
             {isProduction && (
                 <script
-                    dangerouslySetInnerHTML={{
-                        "__html": `
-
-                            window.ssrWasPerformedWithIsDark = "${defaultColorScheme}" === "dark";
-                            
-                            const isDark = (() => {
-                            
-                            	const isDarkExplicitlyProvidedAsParameter = (() => {
-                            		if ("${defaultColorScheme}" === "system") {
-                            			return undefined;
-                            		}
-                            
-                            		switch ("${defaultColorScheme}") {
-                            			case "dark": return true;
-                            			case "light": return false;
-                            		}
-                            	})();
-                            
-                            	const isDarkFromLocalStorage = (() => {
-                            		const colorSchemeReadFromLocalStorage = localStorage.getItem("scheme");
-                            
-                            		if (colorSchemeReadFromLocalStorage === null) {
-                            			return undefined;
-                            		}
-                            
-                            		if (colorSchemeReadFromLocalStorage === "system") {
-                            			return undefined;
-                            		}
-                            
-                            		switch (colorSchemeReadFromLocalStorage) {
-                            			case "dark":
-                            				return true;
-                            			case "light":
-                            				return false;
-                            		}
-                            	})();
-                            
-                            	const isDarkFromOsPreference = (() => {
-                            		if (!window.matchMedia) {
-                            			return undefined;
-                            		}
-                            
-                            		return window.matchMedia("(prefers-color-scheme: dark)").matches;
-                            	})();
-                            
-                            	const isDarkFallback = false;
-                            
-                            	return (
-                            		isDarkFromLocalStorage ??
-                            		isDarkExplicitlyProvidedAsParameter ??
-                            		isDarkFromOsPreference ??
-                            		isDarkFallback
-                            	);
-                            
-                            })();
-                            
-                            ["${data_fr_scheme}", "${data_fr_theme}"].forEach(attr => document.documentElement.setAttribute(attr, isDark ? "dark" : "light"));
-
-                            {
-
-                                const element = document.createElement("style");
-
-                                element.id = "${rootColorSchemeStyleTagId}";
-
-                                element.innerHTML = \`:root { color-scheme: \${isDark ? "dark" : "light"}; }\`;
-
-                                document.head.appendChild(element);
-
-                            }
-
-                            {
-                    
-                                const element = document.createElement("meta");
-                    
-                                element.name = "theme-color";
-                    
-                                element.content = isDark ? "${
-                                    getColors(true).decisions.background.default.grey.default
-                                }" : "${
-                            getColors(false).decisions.background.default.grey.default
-                        }";
-                    
-                                document.head.appendChild(element);
-
-                            }
-				`
-                    }}
-                ></script>
+                    dangerouslySetInnerHTML={{ "__html": getScriptToRunAsap(defaultColorScheme) }}
+                />
             )}
         </>
     );
--- src/next-pagesdir.tsx
+++ src/next-pagesdir.tsx
@@ -6,20 +6,12 @@ import DefaultApp from "next/app";
 import type { AppProps, AppContext } from "next/app";
 import type { DocumentProps, DocumentContext } from "next/document";
 import { rootColorSchemeStyleTagId, data_fr_scheme, data_fr_theme } from "./useIsDark/constants";
+import { getScriptToRunAsap } from "./useIsDark/scriptToRunAsap";
 import { SsrIsDarkProvider } from "./useIsDark/server";
 import type { ColorScheme } from "./useIsDark";
 import { isBrowser } from "./tools/isBrowser";
 import { objectKeys } from "tsafe/objectKeys";
-import marianneLightWoff2Url from "./dsfr/fonts/Marianne-Light.woff2";
-import marianneItalicWoff2Url from "./dsfr/fonts/Marianne-Light_Italic.woff2";
-import marianneRegularWoff2Url from "./dsfr/fonts/Marianne-Regular.woff2";
-import marianneRegularItalicWoff2Url from "./dsfr/fonts/Marianne-Regular_Italic.woff2";
-import marianneMediumWoff2Url from "./dsfr/fonts/Marianne-Medium.woff2";
-import marianneMediumItalicWoff2Url from "./dsfr/fonts/Marianne-Medium_Italic.woff2";
-import marianneBoldWoff2Url from "./dsfr/fonts/Marianne-Bold.woff2";
-import marianneBoldItalicWoff2Url from "./dsfr/fonts/Marianne-Bold_Italic.woff2";
-import spectralRegularWoff2Url from "./dsfr/fonts/Spectral-Regular.woff2";
-import spectralExtraBoldWoff2Url from "./dsfr/fonts/Spectral-ExtraBold.woff2";
+import { fontUrlByFileBasename } from "./next-appdir/fontUrlByFileBasename";
 import AppleTouchIcon from "./dsfr/favicon/apple-touch-icon.png";
 import FaviconSvg from "./dsfr/favicon/favicon.svg";
 import FaviconIco from "./dsfr/favicon/favicon.ico";
@@ -34,19 +26,6 @@ import "./dsfr/dsfr.css";
 import "./dsfr/utility/icons/icons.css";
 
 export type { RegisterLink, RegisteredLinkProps };
-
-const fontUrlByFileBasename = {
-    "Marianne-Light": marianneLightWoff2Url,
-    "Marianne-Light_Italic": marianneItalicWoff2Url,
-    "Marianne-Regular": marianneRegularWoff2Url,
-    "Marianne-Regular_Italic": marianneRegularItalicWoff2Url,
-    "Marianne-Medium": marianneMediumWoff2Url,
-    "Marianne-Medium_Italic": marianneMediumItalicWoff2Url,
-    "Marianne-Bold": marianneBoldWoff2Url,
-    "Marianne-Bold_Italic": marianneBoldItalicWoff2Url,
-    "Spectral-Regular": spectralRegularWoff2Url,
-    "Spectral-ExtraBold": spectralExtraBoldWoff2Url
-} as const;
 
 export type CreateNextDsfrIntegrationApiParams = {
     defaultColorScheme: ColorScheme | "system";
@@ -199,95 +178,7 @@ export function createNextDsfrIntegrationApi(
                         )}
                         <script
                             dangerouslySetInnerHTML={{
-                                "__html": `
-
-                            {
-
-                                window.ssrWasPerformedWithIsDark = "${defaultColorScheme}" === "dark";
-                                
-                                const isDark = (() => {
-                                
-                                	const isDarkExplicitlyProvidedAsParameter = (() => {
-                                		if ("${defaultColorScheme}" === "system") {
-                                			return undefined;
-                                		}
-                                
-                                		switch ("${defaultColorScheme}") {
-                                			case "dark": return true;
-                                			case "light": return false;
-                                		}
-                                	})();
-                                
-                                	const isDarkFromLocalStorage = (() => {
-                                		const colorSchemeReadFromLocalStorage = localStorage.getItem("scheme");
-                                
-                                		if (colorSchemeReadFromLocalStorage === null) {
-                                			return undefined;
-                                		}
-                                
-                                		if (colorSchemeReadFromLocalStorage === "system") {
-                                			return undefined;
-                                		}
-                                
-                                		switch (colorSchemeReadFromLocalStorage) {
-                                			case "dark":
-                                				return true;
-                                			case "light":
-                                				return false;
-                                		}
-                                	})();
-                                
-                                	const isDarkFromOsPreference = (() => {
-                                		if (!window.matchMedia) {
-                                			return undefined;
-                                		}
-                                
-                                		return window.matchMedia("(prefers-color-scheme: dark)").matches;
-                                	})();
-                                
-                                	const isDarkFallback = false;
-                                
-                                	return (
-                                		isDarkFromLocalStorage ??
-                                		isDarkExplicitlyProvidedAsParameter ??
-                                		isDarkFromOsPreference ??
-                                		isDarkFallback
-                                	);
-                                
-                                })();
-                                
-                                ["${data_fr_scheme}", "${data_fr_theme}"].forEach(attr => document.documentElement.setAttribute(attr, isDark ? "dark" : "light"));
-
-                                {
-
-                                    const element = document.createElement("style");
-
-                                    element.id = "${rootColorSchemeStyleTagId}";
-
-                                    element.innerHTML = \`:root { color-scheme: \${isDark ? "dark" : "light"}; }\`;
-
-                                    document.head.appendChild(element);
-
-                                }
-
-                                {
-                    
-                                    const element = document.createElement("meta");
-                    
-                                    element.name = "theme-color";
-                    
-                                    element.content = isDark ? "${
-                                        getColors(true).decisions.background.default.grey.default
-                                    }" : "${
-                                    getColors(false).decisions.background.default.grey.default
-                                }";
-                    
-                                    document.head.appendChild(element);
-
-                                }
-
-                            }
-				`
+                                "__html": getScriptToRunAsap(defaultColorScheme)
                             }}
                         />
                     </Head>
