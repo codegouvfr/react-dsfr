34bccde87d7f1ee1ff2042f51c7803ddc925a9ee
garronej <joseph.garrone@data.gouv.fr>
2023-01-18 00:47:40 +0100

    Use TanStack serializer in color helper

--- stories/ColorHelper/ColorHelper.tsx
+++ stories/ColorHelper/ColorHelper.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useTransition } from "react";
+import React, { useState, useTransition, useRef } from "react";
 import { CallOut } from "../../dist/CallOut";
 import { colorDecisionAndCorrespondingOption } from "../../dist/fr/generatedFromCss/colorDecisionAndCorrespondingOptions";
 import type { ColorDecisionAndCorrespondingOption } from "../../src/scripts/build/cssToTs/colorDecisionAndCorrespondingOptions";
@@ -10,9 +10,12 @@ import { Search } from "./Search";
 import { useConst } from "powerhooks/useConst";
 import { Evt } from "evt";
 import { useStyles } from "./makeStyles";
-import { ColorDecisionCard } from "./ColorDecisionCard";
 import type { Props as SearchProps } from "./Search";
 import { useEffectOnValueChange } from "powerhooks/useEffectOnValueChange";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
+import { ColorDecisionCard } from "./ColorDecisionCard";
+
+const { useDebounce } = createUseDebounce({ "delay": 400 });
 
 export function ColorHelper() {
     const [search, setSearch] = useState("");
@@ -124,6 +127,12 @@ export function ColorHelper() {
                         ? `${filteredColorDecisionAndCorrespondingOption.length} color decisions`
                         : `Found ${filteredColorDecisionAndCorrespondingOption.length} decisions matching your query`}
                 </h3>
+                <RowVirtualizerDynamicWindow
+                    colorDecisionAndCorrespondingOptions={
+                        filteredColorDecisionAndCorrespondingOption
+                    }
+                />
+                {/*
                 {filteredColorDecisionAndCorrespondingOption.map(
                     (colorDecisionAndCorrespondingOption, i) => (
                         <ColorDecisionCard
@@ -133,6 +142,7 @@ export function ColorHelper() {
                         />
                     )
                 )}
+                    */}
             </div>
         </MuiDsfrThemeProvider>
     );
@@ -162,7 +172,65 @@ const usages = Array.from(
     )
 );
 
-const { useDebounce } = createUseDebounce({ "delay": 400 });
+function RowVirtualizerDynamicWindow(props: {
+    colorDecisionAndCorrespondingOptions: readonly ColorDecisionAndCorrespondingOption[];
+}) {
+    const { colorDecisionAndCorrespondingOptions } = props;
+
+    const parentRef = useRef<HTMLDivElement>(null);
+
+    const parentOffsetRef = useRef(0);
+
+    React.useLayoutEffect(() => {
+        parentOffsetRef.current = parentRef.current?.offsetTop ?? 0;
+    }, []);
+
+    const virtualizer = useWindowVirtualizer({
+        "count": colorDecisionAndCorrespondingOptions.length,
+        "estimateSize": () => 45,
+        "scrollMargin": parentOffsetRef.current
+    });
+    const items = virtualizer.getVirtualItems();
+
+    const { css } = useStyles();
+
+    return (
+        <div ref={parentRef}>
+            <div
+                style={{
+                    "height": virtualizer.getTotalSize(),
+                    "width": "100%",
+                    "position": "relative"
+                }}
+            >
+                <div
+                    style={{
+                        "position": "absolute",
+                        "top": 0,
+                        "left": 0,
+                        "width": "100%",
+                        "transform": `translateY(${
+                            items[0].start - virtualizer.options.scrollMargin
+                        }px)`
+                    }}
+                >
+                    {items.map(virtualRow => (
+                        <div
+                            key={virtualRow.key}
+                            data-index={virtualRow.index}
+                            ref={virtualizer.measureElement}
+                        >
+                            <ColorDecisionCard
+                                {...colorDecisionAndCorrespondingOptions[virtualRow.index]}
+                                className={css({ "marginTop": fr.spacing("4v") })}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
 
 const { filterColorDecisionAndCorrespondingOption } = (() => {
     const fzf = new Fzf<readonly ColorDecisionAndCorrespondingOption[]>(
