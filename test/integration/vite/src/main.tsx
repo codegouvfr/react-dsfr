5f69503b5042d81a2632c814f8a212f8571b1417
garronej <joseph.garrone@data.gouv.fr>
2023-02-18 01:26:49 +0100

    Enable external links when using react-router

--- test/integration/vite/src/main.tsx
+++ test/integration/vite/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Home } from "./Home";
 import { Mui } from "./Mui";
-import { startReactDsfr } from "@codegouvfr/react-dsfr";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import { Header } from "@codegouvfr/react-dsfr/Header";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Link, useLocation } from "react-router-dom";
@@ -11,7 +11,7 @@ import { fr } from "@codegouvfr/react-dsfr";
 
 startReactDsfr({ "defaultColorScheme": "system", Link });
 
-declare module "@codegouvfr/react-dsfr" {
+declare module "@codegouvfr/react-dsfr/spa" {
     interface RegisterLink {
         Link: typeof Link;
     }
@@ -36,7 +36,7 @@ function Root() {
                 serviceTitle="Nom du site / service"
                 homeLinkProps={{ "to": "/", "title": "Accueil - Nom de l’entité (ministère, secrétariat d‘état, gouvernement)" }}
                 quickAccessItems={[headerFooterDisplayItem]}
-                navItems={[
+                navigation={[
                     {
                         "text": "Home",
                         "linkProps": {
@@ -50,6 +50,13 @@ function Root() {
                             "to": "/mui"
                         },
                         "isActive": location.pathname === "/mui"
+                    },
+                    {
+                        "text": "External link",
+                        "linkProps": {
+                            "to": "https://example.fr"
+                        },
+                        "isActive": false
                     }
                 ]}
             />
