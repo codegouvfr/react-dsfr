9dba6da7fe700fd7b32aef4ba545a5c08cc5df97
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-03-16 18:33:11 +0100

    Update src/Modal.tsx
    Signed-off-by: Joseph Garrone <joseph.garrone.gj@gmail.com>

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -59,7 +59,7 @@ const Modal = memo(
                 : [buttons_props];
 
         const { t } = useTranslation();
-const titleId= `fr-modal-title-${id}`;
+        const titleId= `fr-modal-title-${id}`;
         return (
             <dialog
                 aria-labelledby={titleId}
