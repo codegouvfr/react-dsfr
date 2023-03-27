a8777bfa3c22fddccd1d596d0753fbbdfb879271
GUEZOU Damien <dguezou@jouve.fr>
2023-03-27 09:18:57 +0200

    fix: fix exports of components blocks

--- src/scripts/list-exports.ts
+++ src/scripts/list-exports.ts
@@ -29,7 +29,7 @@ const newExports = {
     "./tss": "./dist/tss.js",
     "./tools/cx": "./dist/tools/cx.js",
     "./dsfr/*": "./dsfr/*",
-    "./blocks/*": "./dist/blocks/*",
+    "./blocks/*": "./blocks/*",
     ...Object.fromEntries(
         fs
             .readdirSync(srcDirPath)
