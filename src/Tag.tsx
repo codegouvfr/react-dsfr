3c2c2d200cf01b2e0eafe5cb55d94b4191f882f2
Enguerran Weiss <enguerranweiss@gmail.com>
2023-03-28 18:41:16 +0200

    Feature/add tag component (#69)
    * Enable a11y addon for accessibility testing (#67)
    
    Co-authored-by: jjpxiii <jjpxiii@github.com>
    
    * rm unused imports
    
    * rename dismiss -> isDismissible
    
    * mark Tag as done in doc
    
    * DataAttribute type is more readable
    
    * added type test on Tag component
    
    * review fixes: isDismissible, isSmall, test against undefined for non-bool values
    
    ---------
    
    Co-authored-by: Jean-Jérôme Pantalacci <jjpxiii@gmail.com>
    Co-authored-by: jjpxiii <jjpxiii@github.com>

---
+++ src/Tag.tsx
@@ -0,0 +1,180 @@
+import React, {
+    memo,
+    forwardRef,
+    type ReactNode,
+    type RefAttributes,
+    type MemoExoticComponent,
+    type ForwardRefExoticComponent,
+    type CSSProperties
+} from "react";
+import { fr } from "./fr";
+import { cx } from "./tools/cx";
+import type { FrIconClassName, RiIconClassName } from "./fr/generatedFromCss/classNames";
+import { getLink } from "./link";
+import type { RegisteredLinkProps } from "./link";
+import { assert } from "tsafe/assert";
+import type { Equals } from "tsafe";
+import { symToStr } from "tsafe/symToStr";
+
+type DataAttribute = Record<`data-${string}`, string | boolean | null | undefined>;
+
+export type TagProps = TagProps.Common &
+    (TagProps.WithIcon | TagProps.WithoutIcon) &
+    (TagProps.AsAnchor | TagProps.AsButton | TagProps.AsSpan);
+export namespace TagProps {
+    export type Common = {
+        className?: string;
+        /** Default: false */
+        small?: boolean;
+        style?: CSSProperties;
+        title?: string;
+        children: ReactNode;
+    };
+
+    export type WithIcon = {
+        /** Function of the button, to provide if the label isn't explicit */
+        iconId: FrIconClassName | RiIconClassName;
+    };
+
+    export type WithoutIcon = {
+        iconId?: never;
+    };
+
+    export type AsAnchor = {
+        linkProps: RegisteredLinkProps;
+        onClick?: never;
+        nativeButtonProps?: never;
+        nativeSpanProps?: never;
+        dismissible?: never;
+        pressed?: never;
+    };
+    export type AsButton = {
+        linkProps?: never;
+        nativeSpanProps?: never;
+        /** Default: false */
+        dismissible?: boolean;
+        pressed?: boolean;
+        onClick?: React.MouseEventHandler<HTMLButtonElement>;
+        nativeButtonProps?: React.DetailedHTMLProps<
+            React.ButtonHTMLAttributes<HTMLButtonElement>,
+            HTMLButtonElement
+        > &
+            DataAttribute;
+    };
+    export type AsSpan = {
+        linkProps?: never;
+        onClick?: never;
+        dismissible?: never;
+        pressed?: never;
+        nativeButtonProps?: never;
+        nativeSpanProps?: React.DetailedHTMLProps<
+            React.HTMLAttributes<HTMLSpanElement>,
+            HTMLSpanElement
+        > &
+            DataAttribute;
+    };
+}
+
+/** @see <https://react-dsfr-components.etalab.studio/?path=/docs/components-tag> */
+export const Tag = memo(
+    forwardRef<HTMLButtonElement | HTMLAnchorElement | HTMLSpanElement, TagProps>((props, ref) => {
+        const {
+            className: prop_className,
+            children,
+            title,
+            iconId,
+            small = false,
+            pressed,
+            dismissible = false,
+            linkProps,
+            nativeButtonProps,
+            nativeSpanProps,
+            style,
+            onClick,
+            ...rest
+        } = props;
+
+        assert<Equals<keyof typeof rest, never>>();
+
+        const { Link } = getLink();
+
+        const className = cx(
+            fr.cx(
+                "fr-tag",
+                small && `fr-tag--sm`,
+                iconId,
+                iconId && "fr-tag--icon-left", // actually, it's always left but we need it in order to have the icon rendering
+                dismissible && "fr-tag--dismiss"
+            ),
+            linkProps !== undefined && linkProps.className,
+            prop_className
+        );
+
+        return (
+            <>
+                {linkProps !== undefined && (
+                    <Link
+                        {...linkProps}
+                        title={title ?? linkProps.title}
+                        className={cx(linkProps?.className, className)}
+                        style={{
+                            ...linkProps?.style,
+                            ...style
+                        }}
+                        ref={ref as React.ForwardedRef<HTMLAnchorElement>}
+                        {...rest}
+                    >
+                        {children}
+                    </Link>
+                )}
+                {nativeButtonProps !== undefined && (
+                    <button
+                        {...nativeButtonProps}
+                        className={cx(nativeButtonProps?.className, className)}
+                        style={{
+                            ...nativeButtonProps?.style,
+                            ...style
+                        }}
+                        title={title ?? nativeButtonProps?.title}
+                        onClick={onClick ?? nativeButtonProps?.onClick}
+                        disabled={nativeButtonProps?.disabled}
+                        ref={ref as React.ForwardedRef<HTMLButtonElement>}
+                        aria-pressed={pressed}
+                        {...rest}
+                    >
+                        {children}
+                    </button>
+                )}
+                {linkProps === undefined && nativeButtonProps === undefined && (
+                    <span
+                        {...nativeSpanProps}
+                        className={cx(nativeSpanProps?.className, className)}
+                        style={{
+                            ...nativeSpanProps?.style,
+                            ...style
+                        }}
+                        title={title ?? nativeSpanProps?.title}
+                        ref={ref as React.ForwardedRef<HTMLSpanElement>}
+                        {...rest}
+                    >
+                        {children}
+                    </span>
+                )}
+            </>
+        );
+    })
+) as MemoExoticComponent<
+    ForwardRefExoticComponent<
+        TagProps.Common &
+            (TagProps.WithIcon | TagProps.WithoutIcon) &
+            (
+                | (TagProps.AsAnchor & RefAttributes<HTMLAnchorElement>)
+                | (TagProps.AsButton & RefAttributes<HTMLButtonElement>)
+                | (TagProps.AsSpan & RefAttributes<HTMLSpanElement>)
+            )
+    >
+>;
+
+Tag.displayName = symToStr({ Tag });
+
+export default Tag;
