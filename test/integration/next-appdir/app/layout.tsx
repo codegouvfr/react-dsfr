d184d8e6470a42666ae51f9a5067c57067943160
garronej <joseph.garrone@data.gouv.fr>
2023-06-03 03:46:49 +0200

    Scrollbar always visible next appdir test setup

--- test/integration/next-appdir/app/layout.tsx
+++ test/integration/next-appdir/app/layout.tsx
@@ -17,15 +17,22 @@ const brandTop = <>INTITULE<br />OFFICIEL</>;
 const homeLinkPops = { "href": "/", "title": "Accueil - Nom de l’entité (ministère, secrétariat d‘état, gouvernement)" };
 
 declare module "@codegouvfr/react-dsfr/gdpr" {
-    interface RegisterGdprServices { 
-        matomo: never;
-    }
+	interface RegisterGdprServices {
+		matomo: never;
+	}
 }
 
 export default function RootLayout({ children }: { children: JSX.Element; }) {
 
 	return (
-		<html {...getColorSchemeHtmlAttributes({ defaultColorScheme })} >
+		<html
+			{...getColorSchemeHtmlAttributes({ defaultColorScheme })}
+			//NOTE: Scrollbar always visible to avoid layout shift when modal are opened
+			style={{
+				"overflow": "-moz-scrollbars-vertical",
+				"overflowY": "scroll"
+			}}
+		>
 			<head>
 				<title>Next 13 AppDir Demo DSFR setup</title>
 				<StartDsfr />
@@ -53,7 +60,7 @@ export default function RootLayout({ children }: { children: JSX.Element; }) {
 				}}
 			>
 				<DsfrProvider defaultColorScheme={defaultColorScheme}>
-					<ConsentBanner gdprLinkProps={{href: "/mui"}} siteName='Next Test App' services={[
+					<ConsentBanner gdprLinkProps={{ href: "/mui" }} siteName='Next Test App' services={[
 						{
 							name: "matomo",
 							title: "Matomo",
