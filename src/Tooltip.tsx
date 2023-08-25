1084a4979349170aa4ee6dd847176b4dc1621cfe
celineung <celine.ung.pro@gmail.com>
2023-08-25 17:46:02 +0200

    add Tooltip component

---
+++ src/Tooltip.tsx
@@ -0,0 +1,100 @@
+import React, { forwardRef, memo, ReactElement } from "react";
+import type { Equals } from "tsafe";
+import { assert } from "tsafe/assert";
+import { symToStr } from "tsafe/symToStr";
+import { useAnalyticsId } from "./tools/useAnalyticsId";
+
+export type TooltipProps = TooltipProps.WithClickAction | TooltipProps.WithHoverAction;
+
+export namespace TooltipProps {
+    export type Common = {
+        description: string;
+        id?: string;
+        className?: string;
+    };
+
+    export type WithClickAction = Common & {
+        kind: "click";
+        children?: ReactElement | string;
+    };
+
+    export type WithHoverAction = Common & {
+        kind?: "hover";
+        children: ReactElement | string;
+    };
+}
+
+/** @see <https://components.react-dsfr.codegouv.studio/?path=/docs/components-tooltip> */
+export const Tooltip = memo(
+    forwardRef<HTMLSpanElement, TooltipProps>((props, ref) => {
+        const { id: id_prop, className, description, kind, children, ...rest } = props;
+        assert<Equals<keyof typeof rest, never>>();
+
+        const id = useAnalyticsId({
+            "defaultIdPrefix": "fr-tooltip",
+            "explicitlyProvidedId": id_prop
+        });
+
+        const displayChildren = (
+            children: ReactElement | string | undefined,
+            id: string
+        ): ReactElement => {
+            if (children === undefined) return <></>;
+            return typeof children === "string" ? (
+                <span aria-describedby={id} id={`tooltip-owner-${id}`}>
+                    {children}
+                </span>
+            ) : (
+                children &&
+                    React.cloneElement(children, {
+                        "aria-describedby": id,
+                        "id": `tooltip-owner-${id}`
+                    })
+            );
+        };
+
+        return (
+            <>
+                {props.kind === "click" ? (
+                    <span ref={ref}>
+                        {children === undefined ? (
+                            <button
+                                className="fr-btn--tooltip fr-btn"
+                                aria-describedby={id}
+                                id={`tooltip-owner-${id}`}
+                            >
+                                Information contextuelle
+                            </button>
+                        ) : (
+                            displayChildren(children, id)
+                        )}
+                        <span
+                            className={`fr-tooltip fr-placement ${props.className}`}
+                            id={id}
+                            role="tooltip"
+                            aria-hidden="true"
+                        >
+                            {props.description}
+                        </span>
+                    </span>
+                ) : (
+                    <span ref={ref}>
+                        {displayChildren(children, id)}
+                        <span
+                            className={`fr-tooltip fr-placement ${props.className}`}
+                            id={id}
+                            role="tooltip"
+                            aria-hidden="true"
+                        >
+                            {props.description}
+                        </span>
+                    </span>
+                )}
+            </>
+        );
+    })
+);
+
+Tooltip.displayName = symToStr({ Tooltip });
+
+export default Tooltip;
