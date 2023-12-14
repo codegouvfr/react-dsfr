ee455d87e50a2b95a1e6ab9f0c2cc9300ae19b59
Enguerran Weiss <enguerranweiss@gmail.com>
2023-12-14 13:47:15 +0100

    added RadioRichButtons story

--- src/RadioRichButtons.tsx
+++ src/RadioRichButtons.tsx
@@ -4,7 +4,7 @@ import { Fieldset, type FieldsetProps } from "./shared/Fieldset";
 
 export type RadioRichButtonsProps = Omit<FieldsetProps.RadioRich, "type" | "rich">;
 
-/** @see <https://components.react-dsfr.codegouv.studio/?path=/docs/components-radiobutton> */
+/** @see <https://components.react-dsfr.codegouv.studio/?path=/docs/components-radiorichbutton> */
 export const RadioRichButtons = memo(
     forwardRef<HTMLFieldSetElement, RadioRichButtonsProps>((props, ref) => (
         <Fieldset ref={ref} {...props} type="radio" rich />
