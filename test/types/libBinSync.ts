ac260bc181468df012ae3d7d64215a52a212106a
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-12-18 20:04:44 +0100

    Fix broken typelevel test

--- test/types/libBinSync.ts
+++ test/types/libBinSync.ts
@@ -5,6 +5,6 @@ so when we copy declaration values we make sure they stay in sync here
 import { assert } from "tsafe/assert";
 import type { Equals } from "tsafe";
 import type { ColorScheme as ColorScheme_lib } from "../../src/useIsDark";
-import type { ColorScheme as ColorScheme_bin } from "../../src/scripts/build/cssToTs/colorOptions";
+import type { ColorScheme as ColorScheme_bin } from "../../scripts/build/cssToTs/colorOptions";
 
 assert<Equals<ColorScheme_lib, ColorScheme_bin>>();
