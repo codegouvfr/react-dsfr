ee731d7116068ec80660b0e14a8a293bb9f984ef
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2025-06-11 10:58:35 +0200

    Tile picto support test

--- test/integration/vite/src/Picto.tsx
+++ test/integration/vite/src/Picto.tsx
@@ -1,6 +1,13 @@
 import { fr } from "@codegouvfr/react-dsfr";
 
 import * as Pictogrammes from '@codegouvfr/react-dsfr/picto';
+import { type PictoProps } from '@codegouvfr/react-dsfr/picto/utils/PictoWrapper';
+
+const iconColors: PictoProps.Color[] = ["green-tilleul-verveine", "green-bourgeon", "green-emeraude", "green-menthe", "green-archipel", "blue-ecume", "blue-cumulus", "purple-glycine", "pink-macaron", "pink-tuile", "yellow-tournesol", "yellow-moutarde", "orange-terre-battue", "brown-cafe-creme", "brown-caramel", "brown-opera", "beige-gris-galet"];;
+
+function getRandomColor() {
+    return iconColors[Math.floor(Math.random() * iconColors.length)];
+}
 
 export function Picto() {
     return (
@@ -8,11 +15,12 @@ export function Picto() {
             {
                 Object.entries(Pictogrammes).map(([name, Component]) => (
                     <Component
-                    key={name}
-                    fontSize="10em"
+                        key={name}
+                        fontSize="10em"
+                        color={getRandomColor()}
                     />
                 ))
             }
         </div>
-    )
+    );
 }
