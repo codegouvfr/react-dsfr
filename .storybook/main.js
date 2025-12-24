b9159e7052c5f7db6f6b2a673966867f80f31066
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-12-24 15:44:16 +0100

    upgrade from 6 to 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,29 @@
 module.exports = {
+    "framework": {
+        name: "@storybook/nextjs",
+        options: {}
+    },
+
     "stories": [
-        "../stories/*.stories.mdx",
+        "../stories/*.mdx",
         "../stories/*.stories.@(ts|tsx)",
         "../stories/blocks/*.stories.@(ts|tsx)",
         "../stories/charts/*.stories.@(ts|tsx)",
-        "../stories/picto/*.stories.@(ts|tsx)",
+        "../stories/picto/*.stories.@(ts|tsx)"
     ],
+
     "addons": [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "storybook-dark-mode",
-        "@storybook/addon-a11y"
+        "@storybook/addon-a11y",
+        "@storybook/addon-mdx-gfm",
+        "@storybook/addon-webpack5-compiler-swc"
     ],
-    "core": {
-        "builder": "webpack5"
-    },
-    "staticDirs": ["../dist", "./static"]
+
+    "staticDirs": ["../dist", "./static"],
+
+    docs: {
+        autodocs: true
+    }
 };
